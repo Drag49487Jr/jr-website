826a960d2be3f4a84ad8f6ec040b9044655f7563
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-10-16 14:19:54 -0500

    logo renders

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import './NavBar.css';
 
 class NavBar extends Component {
     render() {
         return(
             <>
+            <img className='WolfLogo' src='../images/wolflogo.JPG' alt='wolf' />
             <div>NavBar</div>
             <Link to='/about'>About Me</Link>
             </>
