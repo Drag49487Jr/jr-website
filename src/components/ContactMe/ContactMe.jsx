5f922d14426eed021fba12de2107e1e02f4999ed
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-10-17 18:16:33 -0500

    add contact me component

---
+++ src/components/ContactMe/ContactMe.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import NavBar from '../NavBar/NavBar';
+
+class ContactMe extends Component {
+    render(){
+        return(
+        <div>
+            <NavBar />
+            <div>Contact me</div>
+        </div>
+        )
+    }
+}
+
+export default ContactMe;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,6 +9,7 @@ class NavBar extends Component {
             <Link to='/'> <img className='WolfLogo' src='../images/wolflogo.JPG' alt='wolf'  /></Link>
             <Link className='link' to='/profile'>Profile</Link>
             <Link className='link' to='/projects'>Projects</Link>
+            <Link className='link' to='/contactme'>Contact Me</Link>
             </div>
         )
     }
