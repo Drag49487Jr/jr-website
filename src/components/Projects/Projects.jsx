a2dc78952186f108a2793c9e4eb37502327e0620
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-10-21 15:13:11 -0500

    add pokemon.png

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,7 @@ class Profile extends Component {
             <div>
                 <NavBar />
                 <a href='https://courtkings.herokuapp.com/'><img className='courtking' src='../images/courtking.png' alt='courtking'/></a> 
+                <a href='https://pokibattle.herokuapp.com/'><img className='pokemon' src='../images/pokisearch.png' alt='pokisearch'/></a> 
             </div>
         )
     }
