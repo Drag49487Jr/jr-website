6f84a522551ac2a5e15e169e96fff61b1bb021ad
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-10-21 15:08:11 -0500

    add project css

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import NavBar from '../NavBar/NavBar';
+import './Projects.css';
 
 class Profile extends Component {
     render() {
         return(
             <div>
                 <NavBar />
-                <a href='https://courtkings.herokuapp.com/'><img src='../images/courtking.png' alt='courtking'/></a> 
+                <a href='https://courtkings.herokuapp.com/'><img className='courtking' src='../images/courtking.png' alt='courtking'/></a> 
             </div>
         )
     }
